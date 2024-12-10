241d8929953524d51ca544dc7067d3dfc27967f6
BernardoNogueiraDEV <bernardo.nogueiraff@gmail.com>
2024-12-09 21:57:15 -0300

    atualizando o codigo

--- js/pag3.js
+++ js/pag3.js
@@ -39,3 +39,29 @@ function prevSlide() {
 
 // Inicializa a visibilidade correta no início
 updateVisibility();
+
+
+const valores = document.getElementsByClassName('valor');
+const desconto = document.getElementsByClassName('desconto');
+
+for (let i = 0; i < 12; i++){
+    const valorNumerico = parseFloat(valores[i].innerText.replace('R$ ', '').replace(',', '.'));
+    desconto[i].innerHTML = `Valor a vista com 30% de desconto R$ ${(valorNumerico - (valorNumerico * 0.30)).toFixed(2)}`;
+}
+
+let ativo = false; // Mover a declaração para fora do evento
+
+menu.addEventListener('click', () => {
+    const navegacao = document.getElementById('navegacao');
+    const header = document.getElementById('header');
+    if (ativo === false) {
+        navegacao.style.left = '0';
+        ativo = true;
+        header.style.backdropFilter = 'Blur(12px)';
+    } else {
+        navegacao.style.left = '-100%';
+        ativo = false;
+        header.style.backdropFilter = 'Blur(0px)';
+       
+    }
+});
